f1c6cf5e6426755c315066548ea8f4f30a75757c
michaelsiu <micahel.siu@ideanotion.net>
2013-03-10 19:43:44 -0400

    build.bat change from metropress to ideapress

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ echo "start"
 SET outputPath=c:\builds\ 
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-msbuild %2"MetroPress.sln" /p:Configuration=Release /p:Platform="Any CPU" /p:OutDir=%1
+msbuild %2"IdeaPress.sln" /p:Configuration=Release /p:Platform="Any CPU" /p:OutDir=%1
