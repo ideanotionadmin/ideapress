c3244bbacda86f9103c736c5b91ded1d3768edfe
michaelsiu <micahel.siu@ideanotion.net>
2013-04-17 14:39:39 -0400

    add in json clen up

--- IdeaPress/modules/wordpress/js/wp.module.js
+++ IdeaPress/modules/wordpress/js/wp.module.js
@@ -306,6 +306,7 @@ wordpressModule.prototype.fetch = function (page) {
                     if (data.status != "ok" || data.count == 0) {
                         // no data
                         self.maxPagingIndex = 0;
+                        ideaPress.toggleElement(self.loader, "hide");
                         comp();
                         return;
                     }
@@ -429,14 +430,37 @@ wordpressModule.prototype.getPages = function () {
 };
 
 wordpressModule.prototype.getJsonFromResponse = function (responseText) {
-    var lIndex = responseText.split("").reverse().join("").indexOf(">--");
-    var fIndex = responseText.split("").reverse().join("").indexOf("--!<");
+    if (/^[\],:{}\s]*$/.test(responseText.replace(/\\["\\\/bfnrtu]/g, '@').
+        replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
 
-    if (fIndex >= 0 && lIndex >= 0) {
-        return JSON.parse(responseText.substring(0, responseText.length - fIndex - 4) + responseText.substring(responseText.length - lIndex, responseText.length));
-    } else {
+        //the json is ok
         return JSON.parse(responseText);
-    }
+    } else {
+
+        //the json is not ok
+        /*var lIndex = responseText.split("").reverse().join("").indexOf(">--");
+        var fIndex = resposeText.split("").reverse().join("").indexOf("--!<");
+
+        if (fIndex >= 0 && lIndex >= 0) {
+            return JSON.parse(responseText.substring(0, responseText.length - fIndex - 4) + responseText.substring(responseText.length - lIndex, responseText.length));
+        } else {
+            //fail
+            return JSON.parse(responseText);
+        }*/
+        var startIndex = responseText.indexOf('{"status"');
+
+        if (startIndex < 0)
+        {
+            return JSON.parse("{\"status\":\"fail\"}");
+        }
+        responseText = responseText.substring(startIndex);
+        
+        var regex = /<!--.+?-->/g;
+        responseText = responseText.replace(regex, '');
+        var responseText = responseText;
+        return JSON.parse(responseText);        
+    }    
 };
 
 // Search text using JSON API 
