1524ae39719591b114704958cf21ef5b359ee747
michaelsiu <micahel.siu@ideanotion.net>
2013-05-09 17:19:11 -0400

    remove batch file

--- build.bat
+++
@@ -1,6 +0,0 @@
-echo "start"
-
-SET outputPath=c:\builds\ 
-
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-msbuild %2"IdeaPress.sln" /p:Configuration=Release /p:Platform="Any CPU" /p:OutDir=%1
